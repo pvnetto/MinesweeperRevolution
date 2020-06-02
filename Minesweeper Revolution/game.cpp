0176f73722f779d4e1c5e9b9daa1b1016e5df63e
Paiva <pvnetto1@gmail.com>
2020-06-02 20:15:01 -0300

    add core graphic assets

--- Minesweeper Revolution/game.cpp
+++ Minesweeper Revolution/game.cpp
@@ -14,8 +14,25 @@ int main() {
 
 	// VideoMode: Especifica as configurações de dimensão da tela
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!", windowStyles);
-	sf::CircleShape shape(100.f);
+
+	// Circle
+	float radius = 100.0;
+	sf::CircleShape shape(radius);
 	shape.setFillColor(sf::Color::Green);
+
+	// Texture
+	sf::Texture cellTex;
+	cellTex.loadFromFile("Default.png");
+
+	// Rectangle
+	sf::Vector2f size(50.0, 50.0);
+	sf::Vector2f pos(50.0, 50.0);
+	sf::RectangleShape cell(size);
+	cell.setPosition(pos);
+	cell.setFillColor(sf::Color::Blue);
+	cell.setOrigin(size.x / 2.0, size.y /2.0);
+	cell.setTexture(&cellTex);
+
 
 
 	while (window.isOpen() && !isGameOver) {
@@ -33,8 +50,28 @@ int main() {
 			}
 		}
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			cell.setFillColor(sf::Color(std::rand() * 255.f, std::rand() * 255.f, std::rand() * 255.f));
+		}
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			// Gets mouse position relative to the window
+			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+			sf::Vector2f mousePosF = sf::Vector2f(mousePos.x, mousePos.y);
+			//cell.setPosition(mousePos.x, mousePos.y);
+			if (cell.getGlobalBounds().contains(mousePosF)) {
+				std::cout << "Clicked cell" << std::endl;
+			}
+		}
+
+		// Clears screen buffer
 		window.clear();
+
+		// Draws all shapes
 		window.draw(shape);
+		window.draw(cell);
+
+		// Displays everything that was rendered so far
 		window.display();
 	}
 
